2da262a5acc8f3bbd4eb2e1bbc1c51848f330a8c
giri <you@example.com>
2024-10-02 14:59:46 +0100

    upto VPC peering through terraform cls 31

--- variable/sg.tf
+++ variable/sg.tf
@@ -1,18 +1,8 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t3.micro"
-   vpc_security_group_ids = [aws_security_group.giri-all.id]
-  
-  tags = {
-    Name = "Hello terraform"
-  }
-}
-
 resource "aws_security_group" "giri-all" { # this name giri-all is for terrafrom
     name        = var.sg-name # this name giri-all-aws is for aws SG name.
     description = var.sg-description
   # vpc_id      = aws_vpc.main.id
- 
+
 # resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" 
 
 ingress {
@@ -32,10 +22,4 @@ ingress {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = {
-    Name = "giri-tags-aws"
-    description = "subash VM"
-  }
 }
-
-
